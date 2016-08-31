f293aa4e32f5ded3cc32a75cbe9e2f8223f8e468
fanc <fanaticheng@foxmail.com>
2016-08-31 17:00:56 +0800

    test1

---
+++ test1/test1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test1
+//
+//  Created by fanaticheng on 16/8/31.
+//  Copyright © 2016年 fanaticheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
